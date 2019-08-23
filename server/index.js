9ffd8ca45c91b9700c9196c751a189a3b50a5c96
Tae Kim <taehnkim@gmail.com>
2019-08-23 16:57:28 -0700

    Added GET endpoint for listing image urls

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const db = require('../database/index.js');
 
 const app = express();
 
 app.use(express.static('./public'));
 
 const port = process.env.PORT || 3000;
+
+// get listing images for a given listing id
+app.get('/api/images/:listingid', (req, res) => {
+  db.getImagesForListingId(1, (err, queryResults) => {
+    if (err) console.log(err);
+    else res.send(queryResults.rows);
+  });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
