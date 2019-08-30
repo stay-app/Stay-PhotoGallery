970d333189fb45055c714cc6e0293b0d1114a53d
TK <taehnkim@gmail.com>
2019-08-30 14:19:14 -0700

    Implemented left/right/esc hotkeys

--- client/components/Modal.jsx
+++ client/components/Modal.jsx
@@ -38,16 +38,16 @@ const CarouselWrapper = styled.div`
 const ModalCloseButtonWrapper = styled.div`
   position: absolute;
   right: 0px;
-  margin-top: 40px;
-  margin-right: 40px;
+  margin-top: 20px;
+  margin-right: 20px;
 `;
 
 const ModalCloseButton = styled.button`
   padding: 32px;
   maring: -32px;
-  border:none;
-  background-color:transperant;
-  outline:none;
+  border: none;
+  background-color: transperant;
+  outline: none;
   cursor: pointer;
 `;
 
@@ -71,7 +71,16 @@ class Modal extends React.Component {
     return ReactDOM.createPortal(
       <ModalWrapper>
         <ModalCloseButtonWrapper>
-          <ModalCloseButton onClick={this.props.handleCloseModal}>X</ModalCloseButton>
+          <ModalCloseButton onClick={this.props.handleCloseModal}>
+            <svg
+              viewBox="0 0 24 24"
+              role="img"
+              aria-label="Close"
+              focusable="false"
+              style={{height: '24px', width: '24px', display: 'block', fill: 'rgb(72, 72, 72)'}}>
+              <path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fillRule="evenodd"></path>
+            </svg>
+          </ModalCloseButton>
         </ModalCloseButtonWrapper>
         <MainImageWrapper>
           <MainImage
