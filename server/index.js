ec115db9726c8aed64709072569924b37bc1d535
TK <taehnkim@gmail.com>
2019-09-03 20:42:17 -0700

    Changed port from 3000 to 5000

--- server/index.js
+++ server/index.js
@@ -21,5 +21,5 @@ app.get('/api/images/:listingid', (req, res) => {
   });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
